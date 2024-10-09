da082566834a47d847981867fbdc07b15bd36c30
vladkoisnych <40675670@live.napier.ac.uk>
2024-10-09 17:42:39 +0100

    Testing functionality.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,6 +2,6 @@ package com.napier.sem;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Release 0.1.0.2");
     }
 }
