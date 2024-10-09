d8ee0105ef6d21447358e312bc053f3b09a9f6e4
vladkoisnych <40675670@live.napier.ac.uk>
2024-10-09 18:09:40 +0100

    Adding some comments

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
+# this is a dockerfile with a link to the latest release jar file
 FROM openjdk:latest
 COPY ./target/group_project_25-0.1.0.2-jar-with-dependencies.jar /tmp
 WORKDIR /tmp
--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -2,6 +2,7 @@ package com.napier.sem;
 
 public class App {
     public static void main(String[] args) {
+        //actual code goes here
         System.out.println("Release 0.1.0.2");
     }
 }
