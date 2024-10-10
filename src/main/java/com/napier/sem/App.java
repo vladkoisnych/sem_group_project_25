d9d6f348df9cecefecb81a9167584ddeb461e367
niconowr <40683948@live.napier.ac.uk>
2024-10-10 16:19:34 +0100

    Added java doc.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,6 +1,10 @@
 package com.napier.sem;
 
 public class App {
+    /**
+     * * Main function
+     * * @param args
+     * */
     public static void main(String[] args) {
         //actual code goes here
         System.out.println("Release 0.1.0.2");
