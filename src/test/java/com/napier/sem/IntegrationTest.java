53785b55e8decd7160e644d31d97729ab535fc18
Jason Stewart <40653837@live.napier.ac.uk>
2024-11-17 20:31:24 +0000

    updated GitHub actions workflow to include Integration test

---
+++ src/test/java/com/napier/sem/IntegrationTest.java
@@ -0,0 +1,29 @@
+package com.napier.sem;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IntegrationTest
+{
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        app.connect("localhost:33060", 30000);
+
+    }
+
+    @Test
+    void testGetCity()
+    {
+        City city = app.getCity(1);
+        assertEquals(city.city_id, 255530);
+        assertEquals(city.city_name, "Kabul");
+        assertEquals(city.city_population, 1780000);
+    }
+}
